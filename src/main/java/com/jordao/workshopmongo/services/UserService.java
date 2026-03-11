293a2d3577b8b8f2bcc0b8a859bd1d3427cad474
Jordao Asato <jordaovolletasato@gmail.com>
2026-03-11 12:10:04 -0300

    Inserção de usuário com POST

--- src/main/java/com/jordao/workshopmongo/services/UserService.java
+++ src/main/java/com/jordao/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jordao.workshopmongo.domain.User;
+import com.jordao.workshopmongo.dto.UserDTO;
 import com.jordao.workshopmongo.repositoy.UserRepository;
 import com.jordao.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,16 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
+	}
+	
+	// repositório retorna um obj, por isso mantivemos o padrão de retorno (senão seria void)
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	// pega um DTO e instancia um usuário (caminho inverso)
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 	
 }
