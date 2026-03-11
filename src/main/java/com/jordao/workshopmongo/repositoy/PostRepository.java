a6ddade3704e8219b87a719b4f1a711c7a0b51b9
Jordao Asato <jordaovolletasato@gmail.com>
2026-03-11 18:35:27 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/jordao/workshopmongo/repositoy/PostRepository.java
@@ -0,0 +1,11 @@
+package com.jordao.workshopmongo.repositoy;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jordao.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
