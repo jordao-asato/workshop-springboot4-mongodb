d8465baacc77a9b62021c402c4cf2483eb721555
Jordao Asato <jordaovolletasato@gmail.com>
2026-03-11 12:17:46 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/jordao/workshopmongo/services/UserService.java
+++ src/main/java/com/jordao/workshopmongo/services/UserService.java
@@ -31,6 +31,11 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
 	// pega um DTO e instancia um usuário (caminho inverso)
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
