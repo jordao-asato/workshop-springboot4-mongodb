f6cde884920145c6140b6f83b139ca5e2ae9bc9a
Jordao Asato <jordaovolletasato@gmail.com>
2026-03-12 15:28:51 -0300

    Consulta com vários critérios

--- src/main/java/com/jordao/workshopmongo/resources/PostResource.java
+++ src/main/java/com/jordao/workshopmongo/resources/PostResource.java
@@ -1,6 +1,7 @@
 package com.jordao.workshopmongo.resources;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,22 +22,36 @@ public class PostResource {
 
 	@Autowired
 	private PostService service;
-	
-	@GetMapping(value= "/{id}")
+
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj); // resposta da requisição
 	}
-	
-	@GetMapping(value= "/titlesearch")
-	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
 		try {
 			text = URL.decodeParam(text);
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} // decodifica o texto
 		List<Post> list = service.findByTitle(text);
-		
+
 		return ResponseEntity.ok().body(list); // resposta da requisição
 	}
+
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) throws UnsupportedEncodingException {
+
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list); // resposta da requisição
+	}
+
 }
