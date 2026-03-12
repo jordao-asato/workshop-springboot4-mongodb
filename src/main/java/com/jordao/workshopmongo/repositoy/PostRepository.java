2ef91e427b3f31d69264d1d209a1b859f8cd5a07
Jordao Asato <jordaovolletasato@gmail.com>
2026-03-12 10:37:42 -0300

    Consulta simples com query methods

--- src/main/java/com/jordao/workshopmongo/repositoy/PostRepository.java
+++ src/main/java/com/jordao/workshopmongo/repositoy/PostRepository.java
@@ -1,5 +1,7 @@
 package com.jordao.workshopmongo.repositoy;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.jordao.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/jordao/workshopmongo/resources/PostResource.java
+++ src/main/java/com/jordao/workshopmongo/resources/PostResource.java
@@ -1,13 +1,18 @@
 package com.jordao.workshopmongo.resources;
 
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jordao.workshopmongo.domain.Post;
+import com.jordao.workshopmongo.resources.util.URL;
 import com.jordao.workshopmongo.services.PostService;
 
 @RestController
@@ -21,5 +26,17 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj); // resposta da requisição
+	}
+	
+	@GetMapping(value= "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		try {
+			text = URL.decodeParam(text);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} // decodifica o texto
+		List<Post> list = service.findByTitle(text);
+		
+		return ResponseEntity.ok().body(list); // resposta da requisição
 	}
 }
