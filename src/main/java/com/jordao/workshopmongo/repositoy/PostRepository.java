08ae7d1cc2464f9b491d1973514ea3a815744765
Jordao Asato <jordaovolletasato@gmail.com>
2026-03-12 10:53:18 -0300

    Consulta simples com @Query

--- src/main/java/com/jordao/workshopmongo/repositoy/PostRepository.java
+++ src/main/java/com/jordao/workshopmongo/repositoy/PostRepository.java
@@ -3,12 +3,16 @@ package com.jordao.workshopmongo.repositoy;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jordao.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") // ?0 -> primeiro parâmetro // i -> ignora maiúsculas e minúsculas
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
